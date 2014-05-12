912f98ffc63844b45e30a145010945e577c6ff52
Rodolphe Quiédeville <rodolphe@quiedeville.org>
2014-05-12 17:41:37 +0200

    Remove host checking

--- README.md
+++ README.md
@@ -18,4 +18,15 @@ CONFIGURE
 =========
 
 You'll need to have at least one ssh key present in files/ see the
-file `files/README` to genrate it.
+file `files/README` to generate it.
+
+DISCLAIMER
+==========
+
+By default all tsung hosts will have the same ssh key, you only need
+to have ssh access between the controller to other nodes, it's
+possible to improve this part. To keep this puppet receipe simple we
+only use one key.
+
+Ssh host checking is disabled, if you don't want this, change the
+`files/config` and initialise all connection bettween your nodes.
--- manifests/credentials.pp
+++ manifests/credentials.pp
@@ -1,27 +1,46 @@
 #
 # configure ssh acces auth with key
 #
-class tsung::credentials
-(
-  $username='tsung',
-  $group='tsung'
-)
+class tsung::credentials ($username='tsung', $group='tsung')
 {
   
-  file { '/home/${username}/.ssh':
+  file { "/home/$username/.ssh":
     ensure  => directory,
     group   => $group,
     owner   => $username,
     mode    => 0700,
   }
 
-  file { '/home/${username}/.ssh/authorized_keys':
+  file { "/home/$username/.ssh/authorized_keys":
     ensure  => present,
     group   => $group,
     owner   => $username,
-    mode    => 0700,
+    mode    => 0600,
     source  => "puppet:///modules/tsung/id_dsa.pub"
   }
 
-  
+  file { "/home/$username/.ssh/id_dsa":
+    ensure  => present,
+    group   => $group,
+    owner   => $username,
+    mode    => 0600,
+    source  => "puppet:///modules/tsung/id_dsa"
+  }
+
+  file { "/home/$username/.ssh/id_dsa.pub":
+    ensure  => present,
+    group   => $group,
+    owner   => $username,
+    mode    => 0600,
+    source  => "puppet:///modules/tsung/id_dsa.pub"
+  }
+
+  file { "/home/$username/.ssh/config":
+    ensure  => present,
+    group   => $group,
+    owner   => $username,
+    mode    => 0600,
+    source  => "puppet:///modules/tsung/config"
+  }
+
 }
