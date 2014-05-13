c61d24f6daaad0d11fa63f292ae7093f9e7e832c
Rodolphe Quiédeville <rodolphe@quiedeville.org>
2014-05-13 16:02:28 +0200

    Force export HOME in .profile

--- manifests/install.pp
+++ manifests/install.pp
@@ -28,6 +28,7 @@ class tsung::install
     command => 'git clone -b master https://github.com/processone/tsung.git tsung',
     logoutput => on_failure,
     onlyif => ["test ! -d $dir"],
+    require => Package['git']
     } ->
 
     exec { "git pull":
@@ -35,24 +36,33 @@ class tsung::install
       command => '/bin/sh -c "cd tsung/ ; git pull"',
       logoutput => on_failure,
       onlyif => ["test -d $dir"],
+      require => Package['git']
       } ->
-      
 
-      exec { "./configure in $dir":
-        command => '/bin/sh -c ./configure',
+      # .profile is buggy sometimes and erl failed without HOME set
+      exec { "echo in profile":
+        command => '/bin/bash -c "echo export HOME=/home/tsung >> /home/tsung/.profile"',
         cwd => $dir,
         logoutput => on_failure,
+        } ->
+
+
+      exec { "./configure in $dir":
+        command => '/bin/bash -l -c "./configure"',
+        cwd => $dir,
+        logoutput => on_failure,
+        require => Package['erlang-base','erlang-dev','autoconf']
         } ->
         
         exec { "make in $dir":
    	  cwd => $dir,
-   	  command => 'make',
+   	  command => '/bin/bash -l -c make',
           } ->
           
           exec { "make install in $dir":
    	    cwd => $dir,
    	    user => 'root',
-   	    command => 'make install',
+   	    command => '/bin/bash -l -c make install',
           }
           
 }
